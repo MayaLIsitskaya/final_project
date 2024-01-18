9615850df945e74ceec9af4074d7be226542306a
Maya Lisitskaya <145857366+MayaLIsitskaya@users.noreply.github.com>
2024-01-18 23:46:11 +0300

    added menu-3 page and added styles

---
+++ src/js/accordion.js
@@ -0,0 +1,7 @@
+const accordion = document.querySelector('.accordion-container');
+
+for (i = 0; i<accordion.length; i++) {
+    accordion[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+    })
+}
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,9 +1,17 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 const swiper = new Swiper('.swiper', {
-    loop: true,
+    // effect: "coverflow",
+    // grabCursor: true,
+    // centeredSlides: true,
+    // coverflowEffect: {
+    //     rotate: 0,
+    //     stretch: 0,
+    //     depth: 300,
+    //     slideShadows: false,
+    // },
     scrollbar: {
-        el: '.swiper-scrollbar',
+        el: '.swiper-scrollbar-custom',
     },
 });
 
