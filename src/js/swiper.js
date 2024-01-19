27e37ea56da210d52d0ef6123aac10aaac71b087
Maya Lisitskaya <145857366+MayaLIsitskaya@users.noreply.github.com>
2024-01-19 22:40:11 +0300

    added some more pages and styles

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,17 +1,14 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 const swiper = new Swiper('.swiper', {
-    // effect: "coverflow",
-    // grabCursor: true,
-    // centeredSlides: true,
-    // coverflowEffect: {
-    //     rotate: 0,
-    //     stretch: 0,
-    //     depth: 300,
-    //     slideShadows: false,
-    // },
-    scrollbar: {
-        el: '.swiper-scrollbar-custom',
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button__next',
+        prevEl: '.swiper-button__prev',
     },
 });
 
